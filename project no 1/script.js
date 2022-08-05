826b116178aa996397b1e1adface6819ecce61e7
Pk programmer <pkprogrammer1@gmail.com>
2022-08-05 23:29:00 +0500

    practice on event listener

---
+++ project no 1/script.js
@@ -0,0 +1,34 @@
+const closedFace = document.querySelector(".closed");
+const openedFace = document.querySelector(".open");
+
+// closedFace.addEventListener("click", () => {
+//     if(openedFace.classList.contains("open")){
+//         openedFace.classList.add("active");
+//         closedFace.classList.remove("active");
+//     }
+// })
+// openedFace.addEventListener("click", () => {
+//     if(closedFace.classList.contains("closed")){
+//         closedFace.classList.add("active");
+//         openedFace.classList.remove("active");
+//     }
+// })
+
+
+closedFace.addEventListener('click', () => {
+
+        openedFace.classList.toggle('active');
+
+        closedFace.classList.toggle('active');
+
+})
+
+
+
+openedFace.addEventListener('click', () => {
+
+        openedFace.classList.toggle('active');
+
+        closedFace.classList.toggle('active');
+
+}) 
